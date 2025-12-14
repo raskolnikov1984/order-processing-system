f014ec26fdfef9bc4df6788458a2cdfba6c1c817
aserrador <alejandro.ayala@onecluster.org>
2025-12-14 14:37:24 -0500

    fix: Default create_at

--- order-service/src/order_service/models/database.py
+++ order-service/src/order_service/models/database.py
@@ -1,12 +1,14 @@
 from sqlalchemy.orm import Session
 from .schemas import Order
 from .models import OrderSQL, OrderItemSQL
+import datetime
 
 
 async def db_create_order(order: Order, session: Session) -> OrderSQL:
 
     new_order = OrderSQL(
-        customer_id=order.customer_id
+        customer_id=order.customer_id,
+        created_at=datetime.datetime.now(datetime.UTC)
     )
 
     session.add(new_order)
@@ -17,10 +19,11 @@ async def db_create_order(order: Order, session: Session) -> OrderSQL:
             order_id=new_order.id,
             product_id=item.product_id,
             quantity=int(item.quantity),
+            created_at=datetime.datetime.now(datetime.UTC)
         )
         session.add(order_item)
 
-        await session.commit()
-        await session.refresh(new_order)
+    await session.commit()
+    await session.refresh(new_order)
 
     return new_order
