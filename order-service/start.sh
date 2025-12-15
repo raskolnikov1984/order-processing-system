add1dccc24d3f9e2923d568ece693ef4344b2a4e
aserrador <alejandro.ayala@onecluster.org>
2025-12-15 02:11:22 -0500

    fix: delete revisions alembic

--- order-service/start.sh
+++ order-service/start.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
-alembic revision --autogenerate -m 'Initial Tables'
 alembic upgrade head
 uvicorn src.order_service.main:app --host 0.0.0.0 --port 8010 --reload
--- payment-service/start.sh
+++ payment-service/start.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env sh
-
-alembic revision --autogenerate -m 'Initial Tables'
 alembic upgrade head
 uvicorn src.payment_service.main:app --host 0.0.0.0 --port 8010 --reload
