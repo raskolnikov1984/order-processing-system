e8a39555142c6eb26fe4e186e74534e5b008127e
aserrador <alejandro.ayala@onecluster.org>
2025-12-15 00:04:04 -0500

    fix: Exeception get_inventory_by_product

--- inventory-service/src/inventory_service/api/v1/endpoints/inventory.py
+++ inventory-service/src/inventory_service/api/v1/endpoints/inventory.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from src.inventory_service.api.dependencies import get_async_session
 from src.inventory_service.models.database import (
@@ -16,9 +16,21 @@ router = APIRouter()
 async def inventory(
         product_id: str, session: Session = Depends(get_async_session)):
 
-    result = await db_get_inventory_by_product(product_id, session)
+    try:
+        result = await db_get_inventory_by_product(product_id, session)
+        if result:
+            return InventoryResponse(
+                message="successful",
+                forecast_quantity=result.forecast_quantity
+            )
+    except Exception as e:
+        print(f"Error al crear orden: {str(e)}")
 
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Error al Obetener el Inventario"
+        )
     return InventoryResponse(
-        message="successful",
-        forecast_quantity=result.forecast_quantity
-    )
+            message="Not Found",
+            forecast_quantity=0
+        )
